7e9632704d2bbd74a6f45b7b34463771a0b82883
Lucas Rocha <ld.rocha@hotmail.com>
2020-08-13 14:15:38 -0700

    completed project 6b and challenges

---
+++ Project6b/Project6b/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Lucas Rocha on 2020-08-13.
+//  Copyright © 2020 Lucas Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        var previous: UILabel?
+        
+        for label in  [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            
+            // Setting label height to occupy 1/5 of the view, - 10 for the spacingx
+            label.heightAnchor.constraint(equalToConstant: view.frame.height / 5 - 10).isActive = true
+            
+            // We want the top anchor of the label to be the bottom of the previous one (the one in the above it)
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                // this is just the first label...
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            //Set current label for the next iteration
+            previous = label
+        }
+        
+
+    }
+}
